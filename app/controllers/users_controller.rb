53fac771736b80bba6da30f79b7f127101bdf18b
EC2 Default User <ec2-user@ip-172-31-38-11.ap-northeast-3.compute.internal>
2022-08-07 07:56:42 +0000

    [finish6-4]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user = User.all
     @books = @user.books
     @book = Book.new
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,30 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
   validates :name, length: {in: 2..20}, uniqueness: true, presence: true
   validates :introduction, length: {maximum: 50}
   
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def follow(user_id)
+  relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
   end
 end
